8bca000008197c3845f52a5efc0426cdc2a090b3
Oddo94 <57127779+Oddo94@users.noreply.github.com>
2022-11-13 13:06:39 +0200

    Fix registration bug and data validation bug on insert (#21)
    Performs the following changes:
    -fixes the transition issues between login form, registration form and the main dashboard
    -fixes data validation issue when inserting values for the budget items (changes the Regex pattern)
    -updates code comments

--- BudgetManager/Program.cs
+++ BudgetManager/Program.cs
@@ -14,18 +14,24 @@ namespace BudgetManager {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            LoginForm loginForm = new LoginForm();
-     
-            if(loginForm.ShowDialog() == DialogResult.OK) {
-                int userID = LoginForm.userID;
-                String userName = LoginForm.userName;
+            //LoginForm loginForm = new LoginForm();
 
-                Application.Run(new UserDashboard(userID, userName));
-            } else {
-                //MessageBox.Show("Invalid username and/or password! Please try again", "Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            //if (loginForm.ShowDialog() == DialogResult.OK) {
+            //    int userID = LoginForm.userID;
+            //    String userName = LoginForm.userName;
 
-            //Application.Run(new LoginForm());
+            //    Application.Run(new UserDashboard(userID, userName));
+
+            //    //!LoginForm.isOpeningTheUserRegistrationForm
+            //} else {
+            //    //MessageBox.Show("Invalid username and/or password! Please try again", "Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    if(LoginForm.isOpeningTheUserRegistrationForm) {
+            //        new RegisterForm().Show();
+            //    }
+            //    //Application.Run(new LoginForm());
+            //}
+
+            Application.Run(new LoginForm());
         }
     }
 }
