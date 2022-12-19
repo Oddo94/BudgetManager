a8ddf07ff91c72ca939a0ed531413948b88976be
Oddo94 <razvanodenie@yahoo.com>
2022-12-19 20:04:53 +0200

    Fixes the bugs related to saving account expenses validation on insert

--- BudgetManager/utils/database/DBConnectionManager.cs
+++ BudgetManager/utils/database/DBConnectionManager.cs
@@ -266,6 +266,54 @@ namespace BudgetManager {
 
         }
 
+        //General purpose method used for calling stored procedures from the database
+        public static List<MySqlParameter> callDatabaseStoredProcedure(String procedureName, List<MySqlParameter> inputParameters, List<MySqlParameter> outputParameters) {
+            if (procedureName == null) {
+                throw new ArgumentNullException(procedureName, "The name of the stored procedure cannot be null!");
+            }
+
+            if (inputParameters == null) {
+                throw new ArgumentNullException("input parameter list", "The input parameter list cannot be null!");
+            }
+
+            if (outputParameters == null) {
+                throw new ArgumentNullException("output parameter list", "The output parameter list cannot be null!");
+            }
+
+            MySqlConnection conn = getConnection(BUDGET_MANAGER_CONN_STRING);
+
+            try {
+                MySqlCommand procedureCallingCommand = new MySqlCommand(procedureName, conn);
+                procedureCallingCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+                //Adds the input parameters to the command object
+                foreach (MySqlParameter param in inputParameters) {
+                    param.Direction = System.Data.ParameterDirection.Input;
+                    procedureCallingCommand.Parameters.Add(param);
+                }
+
+                //Sets the type for each output parameter and adds it to the command object
+                foreach (MySqlParameter param in outputParameters) {
+                    param.Direction = System.Data.ParameterDirection.Output;
+                    procedureCallingCommand.Parameters.Add(param);
+                }
+
+                conn.Open();
+
+                //Once the procedure is executed the output parameters are populated with values and the list containing them will be returned from the method
+                procedureCallingCommand.ExecuteNonQuery();
+
+            } catch (MySqlException ex) {
+                throw;
+            } finally {
+                conn.Close();
+            }
+
+
+            return outputParameters;
+
+        }
+
         //Method for checking that the connection with the DB is up and running
         public static bool hasConnection() {
             //A new connection is created 
---
+++ BudgetManager/utils/exceptions/NoDataFoundException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManager.utils.exceptions {
+    [Serializable]
+    public class NoDataFoundException : Exception {
+
+        public string ItemName {
+            get;
+        }
+
+        public NoDataFoundException() { }
+
+        public NoDataFoundException(string message)
+            : base(message) { }
+
+        public NoDataFoundException(string message, Exception inner)
+            : base(message, inner) { }
+
+        public NoDataFoundException(string message, string itemName)
+            : this(message) {
+
+            ItemName = itemName;
+        }
+    }
+}
