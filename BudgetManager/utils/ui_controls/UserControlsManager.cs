6939fa7a991abe727b9b5dc3ba85675288a8b8bb
Oddo94 <57127779+Oddo94@users.noreply.github.com>
2023-04-24 19:15:39 +0300

    User experience improvements (#29)
    * Performs the following changes:
    -adds label that shows the number of displayed results for incomes, expenses, debts and savings
    -adds ordering for the SELECT statements that retrieve data which will be
    used to populate combo boxes
    -adds code for resetting all the form fields after an external account was
    successfully inserted
    -adds logic improvement for checking if all the required fields are
    populated before trying to insert an external account

--- BudgetManager/utils/ui_controls/UserControlsManager.cs
+++ BudgetManager/utils/ui_controls/UserControlsManager.cs
@@ -5,8 +5,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BudgetManager.utils {
